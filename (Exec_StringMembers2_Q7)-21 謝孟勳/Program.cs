143b0d7720378e119161de98c6db469a9efaaf58
shiuns <shiunworks@gmail.com>
2022-10-20 02:12:06 +0800

    加入專案檔案。

---
+++ (Exec_StringMembers2_Q7)-21 謝孟勳/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _Exec_StringMembers2_Q7__21_謝孟勳
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d1 = 0.68;
+            double d2 = 0.88;
+            double sum = (d1 + d2);
+            Console.WriteLine(sum); 
+        }
+    }
+}
